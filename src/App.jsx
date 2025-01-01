d0c10db9862790cc54c27a847fb8ac53595bbb40
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-01 07:35:17 +0530

    Base url changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 
 function App() {
   const [data,setData] = useState("");
 
   useEffect(()=>{
     (async()=>{
-      const resp = await axios.get('http://13.126.231.195:3000');
+      const resp = await axios.get('https://quick-post-be-1.vercel.app/');
       console.log(resp.data);
       setData(resp.data);
 
